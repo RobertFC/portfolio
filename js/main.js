11c08f20c112b7ea88fe69568f62694cfdba7bd1
Beto2203 <lactoz22@gmail.com>
2022-03-10 22:58:51 -0600

    [Improved the delete and write character functions]

--- js/main.js
+++ js/main.js
@@ -6,20 +6,20 @@ let dynamicText = document.querySelector("#dynamicText");
 
 window.addEventListener("resize", changeMenuEvents)
 
-function changeMenuEvents(){
-    if (!window.matchMedia("(min-width: 1000px)").matches){
-        for (item of navList){
+function changeMenuEvents() {
+    if (!window.matchMedia("(min-width: 1000px)").matches) {
+        for (item of navList) {
             item.addEventListener("click", toggleHidden);
         }
     }
-    else{
-        for (item of navList){
+    else {
+        for (item of navList) {
             item.removeEventListener("click", toggleHidden)
         }
     }
 }
 
-function toggleHidden(){
+function toggleHidden() {
     menuList.classList.toggle("hidden");
     menuList.classList.toggle("show");
 }
@@ -30,61 +30,45 @@ menu.addEventListener("click", toggleHidden);
 
 
 
-let web,
-software,
-alternate,
-interval;
+let word,
+    reset,
+    interval;
 
 
-function deleteChar(){
-    dynamicText.textContent = dynamicText.textContent.slice(0,-1)
+function deleteChar() {
+    dynamicText.textContent = dynamicText.textContent.slice(0, -1)
 
-    if (dynamicText.textContent.length === 0){
+    if (dynamicText.textContent.length === 0) {
         clearInterval(interval);
-        if (alternate) start();
+        if (reset) start();
         else {
-            alternate = true;
+            reset = true;
+            word = "Software Engineer";
             interval = setInterval(writeChar, 100);
         }
     }
 }
 
-function writeChar(){
-
-    if (alternate){
-        if (software.length !== 0){
-            dynamicText.textContent += software[0];
-            software = software.slice(1);
-        }
-        else{
-            clearInterval(interval);
-            setTimeout(() => {
-                interval = setInterval(deleteChar, 50);
-            }, 1500)
-        }
+function writeChar() {
+    if (word.length !== 0) {
+        dynamicText.textContent += word[0];
+        word = word.slice(1);
     }
-    else{
-        if (web.length !== 0){
-            dynamicText.textContent += web[0];
-            web = web.slice(1);
-        }
-        else{
-            clearInterval(interval);
-            setTimeout(() => {
-                interval = setInterval(deleteChar, 50);
-            }, 1500)
-        }
+    else {
+        clearInterval(interval);
+        setTimeout(() => {
+            interval = setInterval(deleteChar, 50);
+        }, 1500)
     }
 
 
 }
 
-function start(){
-    web = "Web Developer";
-    software = "Software Engineer";
-    alternate = false;
+function start() {
+    word = "Web Developer";
+    reset = false;
     interval = setInterval(writeChar, 100);
-    
+
 
 }
 
